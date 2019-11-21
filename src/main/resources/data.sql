16e1b24ccd6a03eb053ea88d8ba38b68ad22913d
Rodrigo Ruben Santiago Nieves <nievesnievesnieves@gmail.com>
2019-11-20 19:55:10 -0800

    Insert demo data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Dog (id, name, breed, origin) VALUES (1, 'A','Chihuaha', 'earth');
+INSERT INTO Dog (id, name, breed, origin) VALUES (2, 'B','Miami airport location', 'earth');
+INSERT INTO Dog (id, name, breed, origin) VALUES (3, 'C','Los Angeles airport location', 'earth');
+INSERT INTO Dog (id, name, breed, origin) VALUES (4, 'D','Phoenix airport location', 'earth');
+INSERT INTO Dog (id, name, breed, origin) VALUES (5, 'E','Chicago airport location', 'earth');
